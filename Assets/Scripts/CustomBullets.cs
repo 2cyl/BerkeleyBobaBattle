a9da2cc8e002736f7d263a4d9344f012f50b7ad8
Clare Lin <2cyl@berkeley.edu>
2021-10-04 16:09:02 -0700

    work on projectile and boba bullets

---
+++ Assets/Scripts/CustomBullets.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBullets : MonoBehaviour
+{
+    //assign
+    public Rigidbody rb;
+    public GameObject explosion;
+    public LayerMask whatIsEnemies;
+
+    //stats
+    [Range(0f, 1f)]
+    public float bounciness;
+    public bool useGravity;
+
+    //damage
+    public int explosionDamage;
+    public float explosionRange;
+
+    //lifetime
+    public int maxCollisions;
+    public float maxLifetime;
+    public bool explodeOnTouch = true;
+
+    int collisions;
+    PhysicMaterial physics_mat;
+
+    private void Start()
+    {
+        Setup();
+    }
+
+    private void Update()
+    {
+        //when to explode
+        if (collisions > maxCollisions) Explode();
+
+        //count down
+        maxLifetime -= Time.deltaTime;
+        if (maxLifetime <= 0) Explode();
+    }
+
+    private void Explode()
+    {
+        //instantiate explosion
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+
+        //check enemies
+        Collider[] enemies = Physics.OverlapSphere(transform.position, explosionRange, whatIsEnemies);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            //get component of enemy and call take damage
+            enemies[i].GetComponent<Monster>().Hurt(explosionDamage);
+
+        }
+        Invoke("Delay", 0.05f);
+    }
+
+    private void Delay()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //don't counter collisions
+        if (collision.collider.CompareTag("Bullet")) return;
+
+        //count up collisions
+        collisions++;
+
+        //explode if hits enemy and explodeOnTouch  is activated
+        if (collision.collider.CompareTag("Monster") && explodeOnTouch) Explode(); 
+
+    }
+
+    private void Setup()
+    {
+        //create new Physic material
+        physics_mat = new PhysicMaterial();
+        physics_mat.bounciness = bounciness;
+        physics_mat.frictionCombine = PhysicMaterialCombine.Minimum;
+        physics_mat.bounceCombine = PhysicMaterialCombine.Maximum;
+
+        //assign materal
+        GetComponent<SphereCollider>().material = physics_mat;
+
+        //set gravity
+    }
+}
---
+++ Assets/Scripts/GunProjectile.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GunProjectile : MonoBehaviour
+{
+    //bullet
+    public GameObject bullet;
+    //bullet force
+    public float shootForce, upwardForce;
+    //trigger
+    public GameObject trigger;
+
+    //sound and animation
+    private AudioSource audioSource;
+    private Animator animator;
+    private ParticleSystem particleSystem;
+    public int damage;
+
+    //gun stats
+    public float timeBtwnShooting, spread, reloatTime, timeBtwnShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+    public int reloadTime;
+
+    //recoil
+    public Rigidbody playRb;
+    public float recoilForce;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    //graphics
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+    //bug debug
+    public bool allowInvoke = true;
+
+    private void Awake() {
+        //magazine full
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+
+        audioSource = GetComponent<AudioSource>();
+        animator = transform.Find("Model").GetComponent<Animator>();
+    }
+
+    private void Update() {
+        MyInput();
+
+        //set ammo display
+        if (ammunitionDisplay != null)
+        {
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + "/" +
+                magazineSize / bulletsPerTap);
+        }
+    }
+
+    private void MyInput()
+    {
+        //change shooting to configurable buttons
+
+        //check if can hold down shoot
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //reload
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+        { Reload(); }
+        //reload automatically when shooting without ammo
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0)
+        { Reload(); }
+
+        //shooting
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0) {
+            bulletsShot = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+        audioSource.PlayOneShot(audioSource.clip);
+        animator.SetTrigger("Fire");
+
+        //hit position with raycast
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        //check if ray hits
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject.CompareTag("Monster"))
+            {
+                Monster monsterScript = hitObject.GetComponent<Monster>();
+                monsterScript.Hurt(damage);
+            }
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(75); //far from player
+        }
+
+        //attack to target direction
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+
+        //make bullet/ projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        //rotate bullet
+        currentBullet.transform.forward = directionWithSpread.normalized;
+        //add forces
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+        
+        //muzzle flash
+        if (muzzleFlash != null)
+        {
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        }
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBtwnShooting);
+            allowInvoke = false;
+
+            //recoil
+            playRb.AddForce(-directionWithSpread.normalized * recoilForce, ForceMode.Impulse);
+
+        }
+
+        //shoot more than one bullet per tap
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+        {
+            Invoke("Shoot", timeBtwnShots);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
